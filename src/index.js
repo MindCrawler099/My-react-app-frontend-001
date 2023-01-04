e19ba904a1c30f9245bba4a47c222ac33dcbde2b
MindCrawler099 <botslayer466@gmail.com>
2023-01-04 11:41:05 +0000

    Basic setup to the index.js
    Imported basic react modules and css sheet

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-do/client';
+
+import './sheets/index.css';
+
+const root = document.getElementById('root');
+
+root.render(
+    <React.StrictMode>
+
+    </React.StrictMode>
+)
