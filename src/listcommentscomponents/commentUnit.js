5dcb7bacdb9df7455da422718ecaf0885e1a3b5a
MindCrawler099 <botslayer466@gmail.com>
2023-01-05 14:58:25 +0000

    Added styling to list commens page

---
+++ src/listcommentscomponents/commentUnit.js
@@ -0,0 +1,21 @@
+const CommentUnit =()=>{
+    return(
+        <div className="commentunit-div">
+            <header>
+                <h1>Comment #1</h1>
+            </header>
+            <hr/>
+            <main>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt sequi veritatis asperiores! Incidunt minus dolor voluptates aliquid temporibus, quos excepturi quisquam, voluptatibus hic ullam eius inventore nostrum libero, laudantium soluta!
+                </p>
+            </main>
+            <hr/>
+            <footer>
+                <h2>2002-01-20 03:14:32</h2>
+            </footer>
+
+        </div>
+    )
+}
+
+export default CommentUnit;
--- src/listcommentscomponents/listcommentspagecontent.js
+++ src/listcommentscomponents/listcommentspagecontent.js
@@ -1,7 +1,14 @@
+import CommentUnit from "./commentUnit"
 const ListCommentsPageContent=({currentTitle,setCurrentTitle,currentText,setCurrentText})=>{
     return(
-        <main>
-            {currentTitle}
+        <main id="listcommentspagecontent-main">
+            <CommentUnit/>
+            <CommentUnit/>
+            <CommentUnit/>
+            <CommentUnit/>
+            <CommentUnit/>
+            <CommentUnit/>
+            <CommentUnit/>
         </main>
     )
 }
